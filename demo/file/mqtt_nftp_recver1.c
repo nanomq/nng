91c4db551be841d4b8025683ecb85406c546ecce
wanghaemq <wangwei@emqx.io>
2022-02-23 13:56:42 +0800

    * TMP [recv&json] Add Json parser lib & Modify the process of communicating.

--- demo/file/mqtt_nftp_recver1.c
+++ demo/file/mqtt_nftp_recver1.c
@@ -22,7 +22,6 @@
 #include <nng/supplemental/util/platform.h>
 
 #include <nftp.h>
-#include <cJSON.h>
 
 // File
 #define FILENAME "demo.txt"
@@ -39,10 +38,8 @@ int client_subscribe(nng_socket, nng_mqtt_topic_qos *, int);
 int client_publish(nng_socket, const char *, uint8_t *,uint32_t, uint8_t);
 int mqtt_nftp_recver_start(nng_socket, char *);
 
-// char * nftp_topic_cmd    = "test/room1/emqx/client/user1";
-char * nftp_topic_cmd    = "test/watson/001";
-char * nftp_topic_sender = "file/001";
-char * nftp_topic_recver = "file/001";
+char * nftp_topic_sender = "sender----recver";
+char * nftp_topic_recver = "recver----sender";
 
 static int test_log(void * t) {printf("%s\n", t); return 0;}
 
@@ -52,9 +49,8 @@ main(const int argc, const char **argv)
 	nng_socket sock;
 	nftp_proto_init();
 
-	// const char *url = "mqtt-tcp://127.0.0.1:1883";
-	// const char *url = "mqtt-tcp://broker-cn.emqx.io:1883";
-	const char *url = "mqtt-tcp://112.13.203.122:1883";
+	const char *url = "mqtt-tcp://127.0.0.1:1883";
+	// const char *url = "mqtt-tcp://192.168.23.105:1885";
 
 	client_connect(&sock, url);
 	nng_msleep(1000);
@@ -74,9 +70,9 @@ mqtt_nftp_recver_start(nng_socket sock, char * fname)
 	uint8_t * data;
 	size_t    datasz;
 
-	// nftp recver register
+	// nftp sender register
 	nftp_proto_register(fname, test_log,
-			(void *)"Specfic file [demo.txt] transfer finished.", NFTP_RECVER);
+			(void *)"Demo transfer finished.", NFTP_RECVER);
 	nftp_proto_register("*", NULL, NULL, NFTP_RECVER);
 
 	// nng-mqtt sub for recving ack
@@ -88,15 +84,8 @@ mqtt_nftp_recver_start(nng_socket sock, char * fname)
 				.length = strlen(nftp_topic_sender)
 			}
 		},
-		{
-			.qos   = 1,
-			.topic = {
-				.buf    = nftp_topic_cmd,
-				.length = strlen(nftp_topic_cmd)
-			}
-		},
 	};
-	client_subscribe(sock, subscriptions, 2);
+	client_subscribe(sock, subscriptions, 1);
 }
 
 nng_mqtt_cb user_cb = {
@@ -150,10 +139,9 @@ client_connect(nng_socket *sock, const char *url)
 	nng_mqtt_msg_set_packet_type(connmsg, NNG_MQTT_CONNECT);
 	nng_mqtt_msg_set_connect_proto_version(connmsg, 4);
 	nng_mqtt_msg_set_connect_keep_alive(connmsg, 60);
+	nng_mqtt_msg_set_connect_will_msg(connmsg, "bye-bye", strlen("bye-bye"));
+	nng_mqtt_msg_set_connect_will_topic(connmsg, "will_topic");
 	nng_mqtt_msg_set_connect_clean_session(connmsg, true);
-	nng_mqtt_msg_set_connect_client_id(connmsg, "3c988e4b4ef60f77513a24bbe7699527");
-	nng_mqtt_msg_set_connect_user_name(connmsg, "KZXQZI8T-user4");
-	nng_mqtt_msg_set_connect_password(connmsg, "ce65b5a71b5a0685a528376a13d553d7");
 
 	uint8_t buff[1024] = { 0 };
 
@@ -161,6 +149,8 @@ client_connect(nng_socket *sock, const char *url)
 	nng_dialer_set_ptr(dialer, NNG_OPT_MQTT_CONNMSG, connmsg);
 	nng_dialer_set_cb(dialer, &user_cb);
 	nng_dialer_start(dialer, NNG_FLAG_NONBLOCK);
+
+	printf("connected\n");
 
 	// TODO Connmsg would be free when client disconnected
 	return (0);
@@ -197,94 +187,32 @@ client_subscribe(nng_socket sock, nng_mqtt_topic_qos *subscriptions, int count)
 		uint32_t payload_len;
 		uint8_t *retpayload;
 		uint32_t retpayload_len;
-		char * topic, *topicTmp;
-		uint32_t topic_len;
-		cJSON * cmdjson = NULL;
-		cJSON * cmd_str = NULL, *recvpath_str = NULL, *filename_str = NULL;
 
-		if ((rv = nng_recvmsg(sock, &msg, 0)) != 0)
+		if ((rv = nng_recvmsg(sock, &msg, 0)) != 0) {
 			fatal("nng_recvmsg", rv);
+		}
 
 		// we should only receive publish messages
-		if (nng_mqtt_msg_get_packet_type(msg) != NNG_MQTT_PUBLISH)
-			continue;
-		
-		topicTmp = nng_mqtt_msg_get_publish_topic(msg, &topic_len);
-		topic = strndup(topicTmp, topic_len);
+		assert(nng_mqtt_msg_get_packet_type(msg) == NNG_MQTT_PUBLISH);
+
 		payload = nng_mqtt_msg_get_publish_payload(msg, &payload_len);
 
-		printf("\ntopic is %s. \n", topic);
-		printf("payload is %s. \n\n", payload);
-		if (0 == strcmp(topic, nftp_topic_cmd)) {
-			cmdjson = cJSON_Parse(payload);
-			if (NULL == cmdjson) {
-				char *error_ptr = cJSON_GetErrorPtr();
-				printf("Error before: %s\n", error_ptr);
-				goto next;
-			}
-
-			cmd_str = cJSON_GetObjectItemCaseSensitive(cmdjson, "cmd");
-			if (cJSON_IsNull(cmd_str)) {
-				printf("cmd is null\n");
-				goto next;
-			}
-			if (!cJSON_IsString(cmd_str)) {
-				printf("cmd is not string [%d]\n", cmd_str->type);
-				goto next;
-			}
-			if (0 != strcmp("recv", cmd_str->valuestring)) {
-				printf("type is not recv [%s]\n", cmd_str->valuestring);
-				goto next;
-			}
-
-			recvpath_str = cJSON_GetObjectItemCaseSensitive(cmdjson, "recvpath");
-			if (!cJSON_IsString(recvpath_str)) {
-				printf("recvpath is not string\n");
-				goto next;
-			}
-			printf("recvpath is %s\n", recvpath_str->valuestring);
-			if (0 == strcmp("", recvpath_str->valuestring))
-				nftp_set_recvdir("./");
-			else
-				nftp_set_recvdir(recvpath_str->valuestring);
-
-			filename_str = cJSON_GetObjectItemCaseSensitive(cmdjson, "filename");
-			if (!cJSON_IsString(filename_str)) {
-				printf("filename is not string\n");
-				goto next;
-			}
-			printf("filename is %s\n", filename_str->valuestring);
-			goto next;
-		}
-
 		// nftp protocol handler
-		rv = 1;
-		if (0 == strcmp(topic, nftp_topic_sender)) {
-			printf("nftp handler start.\n");
-			rv = nftp_proto_handler(payload, payload_len, &retpayload, &retpayload_len);
+		rv = nftp_proto_handler(payload, payload_len, &retpayload, &retpayload_len);
+		if (rv == 0) {
+			if (retpayload != NULL && retpayload_len != 0) {
+				printf("Recved NFTP_HELLO\n");
+				client_publish(sock, nftp_topic_recver, retpayload, retpayload_len, 1);
+				printf("Sent NFTP_ACK\n");
+			} else {
+				printf("Recved NFTP_FILE/END\n");
+			}
+			payload = NULL;
+			payload_len = 0;
 		}
-
-		if (rv != 0) {
-			printf("Abandon the msg (Not comply to NFTP)\n");
-			goto next;
-		}
-
-		if (retpayload != NULL && retpayload_len != 0) {
-			printf("Recved NFTP_HELLO\n");
-			client_publish(sock, nftp_topic_recver, retpayload, retpayload_len, 1);
-			printf("Sent NFTP_ACK\n");
-		} else {
-			printf("Recved NFTP_FILE/END\n");
-		}
-
-next:
-		free(topic);
-		payload = NULL;  topic = NULL;
-		payload_len = 0; topic_len = 0;
+		nng_msleep(200);
 
 		nng_msg_free(msg);
-		cJSON_Delete(cmdjson);
-		nng_msleep(200);
 	}
 
 	return rv;
